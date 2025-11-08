0f796813aa8ca1b3a1b02b5420d3bdccde7d4a52
JungleMango <mrafideen2@gmail.com>
2025-11-08 19:05:49 +0000

    1.2.1

--- pages/04_🔎_Stock_Analysis.py
+++ pages/04_🔎_Stock_Analysis.py
@@ -1,12 +1,13 @@
+# streamlit_app.py
+
 import streamlit as st
-import pandas as pd
+from streamlit_gsheets import GSheetsConnection
 
-First_Table = pd.DataFrame(
-    [
-        {"Ticker": "NVDA", "price": 4, "is_widget": True},
-        {"Ticker": "TSLA", "price": 5, "is_widget": False},
-        {"Ticker": "QQQ", "price": 3, "is_widget": True},
-    ]
-)
+# Create a connection object.
+conn = st.connection("gsheets", type=GSheetsConnection)
 
-st.dataframe(First_Table, use_container_width=True,hide_index=True)
+df = conn.read()
+
+# Print results.
+for row in df.itertuples():
+    st.write(f"{row.name} has a :{row.pet}:")
