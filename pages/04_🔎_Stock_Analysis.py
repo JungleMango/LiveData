4836ef3b13aee4d3974ab3a947b497d71521c8d9
JungleMango <mrafideen2@gmail.com>
2025-11-08 20:08:52 +0000

    1.2.5

--- pages/04_🔎_Stock_Analysis.py
+++ pages/04_🔎_Stock_Analysis.py
@@ -1,12 +1,10 @@
-# streamlit_app.py
-
 import streamlit as st
-from streamlit_gsheets import GSheetsConnection
+from st_gsheets_connection import GSheetsConnection  # recommended package
 
-# Create a connection object.
 conn = st.connection("gsheets", type=GSheetsConnection)
 
 df = conn.read(
+    spreadsheet="https://docs.google.com/spreadsheets/d/1CKDZqZIZ6WTbHGRuTf47DUxtlsSrBC5buZ6u8soQ-rw/edit",  # or just "YOUR_SHEET_ID"
     worksheet="Watchlist",
     ttl="10m",
     usecols=[0, 1],
