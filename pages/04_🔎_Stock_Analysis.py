c970634587f1a42361c9ff75a377e62e5c8c436a
JungleMango <mrafideen2@gmail.com>
2025-11-12 19:43:22 +0000

    1.28

--- pages/04_🔎_Stock_Analysis.py
+++ pages/04_🔎_Stock_Analysis.py
@@ -5,7 +5,8 @@ import pandas as pd
 api_key = 'beUiETWAQ7Ert13VnAd7qkiEqjT1GrFC'
 base_url = 'https://financialmodelingprep.com'
 data_type = 'income-statement'
-ticker = 'AAPL'
+print("Input ticker symbol")
+ticker = st.text_input()
 url = f'{base_url}/stable/{data_type}?symbol={ticker}&apikey={api_key}'
 
 response = requests.get(url)
