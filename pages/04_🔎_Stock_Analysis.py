7b9420af8020d50e8f8feb16fa0d8b271e4077f2
JungleMango <mrafideen2@gmail.com>
2025-11-13 18:48:53 +0000

    ee

--- pages/04_🔎_Stock_Analysis.py
+++ pages/04_🔎_Stock_Analysis.py
@@ -7,30 +7,30 @@ base_url = 'https://financialmodelingprep.com'
 data_type = 'income-statement'
 ticker = st.text_input("Enter Ticker")
 
-
+#----------------------------#
+    # DECLARING FUNCTIONS 
+#----------------------------#
 
 # Creating a function to witch im giving a variable to it. 
 # Function will return the data from specified website.
 @st.cache_data(ttl=86400)
 def fetch_income(ticker):
-    Inc_Stat_Url = f'{base_url}/stable/{data_type}?symbol={ticker}&apikey={api_key}' # variable to created url
+    Inc_Stat_Url = f'{base_url}/stable/{data_type}?symbol={ticker}?period=quarter&apikey={api_key}' # variable to created url
     Income = requests.get(Inc_Stat_Url) # assigning variable to data requested from website
     return Income.json() # saving result as json
 
 
-
-
+#----------------------------#
+    # EXECUTING FUNCTIONS 
+#----------------------------#
 
 IST = pd.DataFrame(fetch_income(ticker))
 
+#----------------------------#
+    # UI / STYLING 
+#----------------------------#
 
-
-
-## UI ##
 st.subheader("Income statement")
-
 st.dataframe(IST)
-
 EPS = IST['eps']
-
 EPS
