21a610ac02a8ab5bb6ee4ffa29adbb36e5aae670
JungleMango <mrafideen2@gmail.com>
2025-11-13 05:15:03 +0000

    nbjb

--- pages/04_🔎_Stock_Analysis.py
+++ pages/04_🔎_Stock_Analysis.py
@@ -7,26 +7,25 @@ base_url = 'https://financialmodelingprep.com'
 data_type = 'income-statement'
 ticker = st.text_input("Enter Ticker")
 
-Inc_Stat_Url = f'{base_url}/stable/{data_type}?symbol={ticker}&apikey={api_key}'
-Quote_Url = f'{base_url}/stable/quote?symbol={ticker}&apikey=beUiETWAQ7Ert13VnAd7qkiEqjT1GrFC'
 
+# Quote_Url = f'{base_url}/stable/quote?symbol={ticker}&apikey=beUiETWAQ7Ert13VnAd7qkiEqjT1GrFC'
 
-Live_Quote = requests.get(Quote_Url)
-Income_Statement = requests.get(Inc_Stat_Url)
-
-
-Quote_Price = Live_Quote.json()
-
-Income_Statement_Table = Income_Statement.json()
+@st.cache_data(ttl=86400)
+def fetch_income(ticker):
+    Inc_Stat_Url = f'{base_url}/stable/{data_type}?symbol={ticker}&apikey={api_key}'
+    return requests.get(Inc_Stat_Url).json()
 
 
 
-QP = pd.DataFrame(Quote_Price).T
-IST = pd.DataFrame(Income_Statement_Table).T
+
+
+IST = pd.DataFrame(fetch_income).T
+
+
 
 
 ## UI ##
 st.subheader("Live Quote")
-st.table(QP)
+st.table(QP, border=("horizontal"))
 st.subheader("Income statement of the company")
 st.table(IST, border=("horizontal"))
