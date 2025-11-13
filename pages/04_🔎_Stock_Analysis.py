6c91d22511aea5f7ba1d736a5eceab2f8bfb0f92
JungleMango <mrafideen2@gmail.com>
2025-11-13 20:04:12 +0000

    kk

--- pages/04_🔎_Stock_Analysis.py
+++ pages/04_🔎_Stock_Analysis.py
@@ -16,13 +16,14 @@ N = '30'
 # Function will return the data from specified website.
 @st.cache_data(ttl=86400)
 def fetch_income(ticker):
-    Inc_Stat_Url = f'{base_url}/api/v3/{data_type}/{ticker}?limit={N}&apikey={api_key}' # variable to created url
+    Inc_Stat_Url = f'{{base_url}/stable/{data_type}?symbol={ticker}&period=quarter&limit={N}&apikey={api_key}' # variable to created url
     Income = requests.get(Inc_Stat_Url) # assigning variable to data requested from website
     return Income.json() # saving result as json
 
 
-
-# {base_url}/stable/{data_type}?symbol={ticker}&apikey={api_key}
+# 
+# https://financialmodelingprep.com/stable/analyst-estimates?symbol=NVDA&period=quarter&limit=50&apikey=beUiETWAQ7Ert13VnAd7qkiEqjT1GrFC
+# {base_url}/stable/{data_type}?symbol={ticker}&period=quarter&limit={N}&apikey={api_key}
 
 #----------------------------#
     # EXECUTING FUNCTIONS 
