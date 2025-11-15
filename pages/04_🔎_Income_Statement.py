4f9b396769376672a2294d93ceade565f7a9a59e
JungleMango <mrafideen2@gmail.com>
2025-11-15 17:25:52 +0000

    ds

--- pages/04_🔎_Income_Statement.py
+++ pages/04_🔎_Income_Statement.py
@@ -36,8 +36,8 @@ st.write(Income_statement_table)
 Quote_table = pd.DataFrame(fetch_quote(ticker))
 EPS_table = Income_statement_table[["date","eps"]]
 
-EPS_table["Date"] = pd.to_datetime(EPS_table["Date"])
-Quote_table["Date"] = pd.to_datetime(Quote_table["Date"])
+EPS_table["Date"] = pd.to_datetime(EPS_table["date"])
+Quote_table["Date"] = pd.to_datetime(Quote_table["date"])
 EPS_table = EPS_table.sort_values("Date")
 Quote_table = Quote_table.sort_values("Date")
 
