893d830f4021dd5d2a9800f08bbfaa47cc150ab6
JungleMango <mrafideen2@gmail.com>
2025-11-15 17:17:37 +0000

    234

--- pages/04_🔎_Income_Statement.py
+++ pages/04_🔎_Income_Statement.py
@@ -32,6 +32,7 @@ def fetch_quote(ticker):
 #----------------------------#
 
 Income_statement_table = pd.DataFrame(fetch_quote(ticker))
+Income_statement_table
 Quote_table = pd.DataFrame(fetch_quote(ticker))
 EPS_table = Income_statement_table[["Date","Eps"]]
 
