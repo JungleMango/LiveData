bcfef4051476e813761ae6fd9903f8568e7cff25
JungleMango <mrafideen2@gmail.com>
2025-11-17 17:03:35 +0000

    th

--- pages/05_Gold.py
+++ pages/05_Gold.py
@@ -9,7 +9,7 @@ import pandas as pd
 
 @st.cache_data(ttl=15)
 def fetch_live_gold():
-    Live_Gold_Url = https://financialmodelingprep.com/stable/quote?symbol=XAUUSD&apikey=beUiETWAQ7Ert13VnAd7qkiEqjT1GrFC
+    Live_Gold_Url = 'https://financialmodelingprep.com/stable/quote?symbol=XAUUSD&apikey=beUiETWAQ7Ert13VnAd7qkiEqjT1GrFC'
     Gold_Price = requests.get(Live_Gold_Url)
     return Gold_Price.json()
 
