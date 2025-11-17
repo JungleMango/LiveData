48493021d18a32b31844f5ec2d059a379dc4a790
JungleMango <mrafideen2@gmail.com>
2025-11-17 17:18:11 +0000

    dd

--- pages/05_Gold.py
+++ pages/05_Gold.py
@@ -14,12 +14,32 @@ def fetch_live_gold():
     Gold_Price = requests.get(Live_Gold_Url)
     return Gold_Price.json()
 
-st.write(fetch_live_gold())
-
 #----------------------------#
     # EXECUTING FUNCTIONS #
 #----------------------------#
 
 Gold_info = fetch_live_gold()
 Gold_Price = Gold_info[0]["price"]
-st.write(Gold_Price)
+
+#----------------------------#
+    # UI #
+#----------------------------#
+
+st.write(f"Live Price — ${Gold_Price:,.2f}")
+st.markdown(
+        f"""
+        <span style="
+            padding: 18px;
+            border-radius: 12px;
+            text-align: center;
+            color: white;
+            font-size: 14px;
+            font-weight: 700;
+            box-shadow: 0 4px 12px rgba(0,0,0,0.25);
+            letter-spacing: 1px;
+        ">
+            "Gold price — ${Gold_Price:,.2f}
+        </span>
+        """,
+        unsafe_allow_html=True
+    )
