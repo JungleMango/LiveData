e7c764f023cdd984c0762811ab02ec86c56f791b
JungleMango <mrafideen2@gmail.com>
2025-11-17 17:02:47 +0000

    kk

---
+++ pages/05_Gold.py
@@ -0,0 +1,16 @@
+import streamlit as st
+import requests
+import pandas as pd 
+
+
+
+
+
+
+@st.cache_data(ttl=15)
+def fetch_live_gold():
+    Live_Gold_Url = https://financialmodelingprep.com/stable/quote?symbol=XAUUSD&apikey=beUiETWAQ7Ert13VnAd7qkiEqjT1GrFC
+    Gold_Price = requests.get(Live_Gold_Url)
+    return Gold_Price.json()
+
+st.write(fetch_live_gold)
