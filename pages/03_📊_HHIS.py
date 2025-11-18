7f167d10b436cc3f90053b7afc019e4069ea784d
JungleMango <mrafideen2@gmail.com>
2025-11-18 17:20:26 +0000

    asd

--- pages/03_📊_HHIS.py
+++ pages/03_📊_HHIS.py
@@ -29,6 +29,6 @@ def fetch_histo_quotes(ticker):
 #----------------------------#
 
 All_Quotes = fetch_histo_quotes(ticker)
-Ticker_Price_log = pd.dataFrame(All_Quotes)
+Ticker_Price_log = pd.DataFrame(All_Quotes)
 
 Ticker_Price_log
